8ad9e5608cc5fbfa1342a5ad0eb766c36efe8f9a
Jan-Niclas Struewer <jan-niclas.struewer@iem.fraunhofer.de>
2020-09-02 16:29:15 +0200

    api calls are now platform dependent, because the ios and android simulator work on different adresses

--- app/api/ApiFactory.ts
+++ app/api/ApiFactory.ts
@@ -1,11 +1,18 @@
 import {DefaultApi, Configuration} from './openApi';
+import {Platform} from 'react-native';
 
 export default class ApiFactory {
   private static api: DefaultApi;
 
   public static getInstance() {
     if (!ApiFactory.api) {
-      ApiFactory.api = new DefaultApi(new Configuration({basePath: 'http://10.0.2.2:3000'}));
+      // the ios and android simulator have different ip adresses
+      const platformBasePath = Platform.select({
+        ios: () => 'http://localhost:3000',
+        android: () => 'http://10.0.2.2:3000',
+        default: () => '',
+      })();
+      ApiFactory.api = new DefaultApi(new Configuration({basePath: platformBasePath}));
     }
     return this.api;
   }
